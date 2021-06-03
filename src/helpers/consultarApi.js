48aa5f98d5043f0bf0879f0e1df8eb84dfe49689
Jonathan Ruiz <jruiz.967@gmail.com>
2021-06-02 22:28:12 -0500

    Proyecto finalizado

--- src/helpers/consultarApi.js
+++ src/helpers/consultarApi.js
@@ -1,6 +1,6 @@
 export const consultarApi = async (ciudad, pais) => {
 
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURI(ciudad)},${encodeURI(pais)}&appid=04ee0a10734384ca4d576c325178b190`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(ciudad)},${encodeURI(pais)}&appid=04ee0a10734384ca4d576c325178b190`;
     
     try {
       const response = await fetch(url);
